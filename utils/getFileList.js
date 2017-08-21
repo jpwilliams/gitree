983884b38251bb7e302f9106e67dae692850c1a5
Jack Williams <jpwilliamsphotography@gmail.com>
2017-08-21 10:51:12 +0100

    ✨ Also list untracked files

--- utils/getFileList.js
+++ utils/getFileList.js
@@ -3,6 +3,23 @@ const processFiles = require('./processFiles')
 const cmd = 'git ls-files --exclude-standard --directory --no-empty-directory'
 
 async function getFileList (onlyModified) {
+  const files = await Promise.all([
+    getGitFiles(onlyModified),
+    getUntrackedFiles()
+  ])
+
+  const sortedFiles = files
+    .join('\n')
+    .trim()
+    .split('\n')
+    .sort()
+
+  const data = processFiles(sortedFiles)
+
+  return data
+}
+
+async function getGitFiles (onlyModified) {
   try {
     var { stdout: filesOut } = await execa.shell(cmd + (onlyModified ? ' -m' : ''))
   } catch (e) {}
@@ -10,12 +27,18 @@ async function getFileList (onlyModified) {
   if (!filesOut) {
     console.log('No files or no git repository found.')
 
-    return []
+    return ''
   }
 
-  const data = processFiles(filesOut)
+  return filesOut
+}
 
-  return data
+async function getUntrackedFiles () {
+  try {
+    var { stdout: filesOut } = await execa.shell(cmd + ' -o')
+  } catch (e) {}
+
+  return filesOut || ''
 }
 
 module.exports = getFileList
--- utils/processFiles.js
+++ utils/processFiles.js
@@ -3,7 +3,6 @@ const getFileTarget = require('./getFileTarget')
 
 function processFiles (x) {
   return x
-    .split('\n')
     .map((file) => {
       return './' + file
     })
