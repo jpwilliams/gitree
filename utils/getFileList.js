ef7026302027dbec918ae15dc7b37a0c967e3ecc
Jack Williams <jpwilliamsphotography@gmail.com>
2017-08-22 13:47:09 +0100

    🐛 Fix undefined error thrown if no git repository found and no -m flag used

--- utils/getFileList.js
+++ utils/getFileList.js
@@ -6,6 +6,10 @@ async function getFileList () {
     var { stdout: files } = await execa.shell(cmd)
   } catch (e) {}
 
+  if (!files) {
+    return []
+  }
+
   return files.split('\n').sort()
 }
 
