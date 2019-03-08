4a18ce6ebfb9017c512506ccae0a542400b7cc15
Jack Williams <jpwilliamsphotography@gmail.com>
2019-03-08 12:30:23 +0000

    optionally exclude non-tracked files when building nodes

--- utils/buildNodes.js
+++ utils/buildNodes.js
@@ -1,16 +1,20 @@
 const path = require('path')
 const getGitRoot = require('./getGitRoot')
 
-async function buildNodes (files, statuses, p) {
+async function buildNodes (files, statuses, p, trackedOnly) {
   const gitRoot = await getGitRoot(p)
 
-  files = files.map((file) => {
+  files = files.reduce((list, file) => {
     const ret = {
       to: path.join(gitRoot, file),
       from: null
     }
 
     if (statuses[file]) {
+      if (trackedOnly && [statuses[file].x, statuses[file].y].includes('?')) {
+        return list
+      }
+
       if (statuses[file].from) {
         ret.from = path.join(gitRoot, statuses[file].from)
       }
@@ -21,11 +25,17 @@ async function buildNodes (files, statuses, p) {
       delete statuses[file]
     }
 
-    return ret
-  })
+    list.push(ret)
+
+    return list
+  }, [])
 
   if (Object.keys(statuses).length) {
     Object.keys(statuses).forEach((status) => {
+      if (trackedOnly && [statuses[status].x, statuses[status].y].includes('?')) {
+        return
+      }
+
       files.push({
         to: path.join(gitRoot, statuses[status].to),
         from: statuses[status].from ? path.join(gitRoot, statuses[status].from) : null,
